04cc058b04d2cdf132f21fa9c965c4ff6675e0ad
Ветров Сергей <vetrov241201@yandex.ru>
2020-12-10 17:32:20 +0300

    Создал класс ComputerCase и его некоторые поля

--- computercase.hpp
+++ computercase.hpp
@@ -1,11 +1,50 @@
 #ifndef COMPUTERCASE_HPP
 #define COMPUTERCASE_HPP
 
+#include <iostream>
 
 class ComputerCase
 {
+    struct Size
+    {
+        double width;
+        double height;
+        double depth;
+    };
+    enum Color
+    {
+            DEFAILT = -1,
+            BLACK,
+            DARKBLUE,
+            DARKGREEN,
+            DARKCYAN,
+            DARKRED,
+            DARKMAGENTA,
+            DARKYELLOW,
+            DARKGRAY,
+            GRAY ,
+            BLUE,
+            GREEN,
+            CYAN,
+            RED,
+            MAGENTA,
+            YELLOW,
+            WHITE
+    };
+
+private:
+    std::string _name;
+    std::string _operationSystem;
+    Size _size;
+    Color _color;
+    bool _isDvdDrive;
+    std::string _soundSystem;
+    int _SSDcapacity;
+    double _weight;
+
 public:
     ComputerCase();
+    ~ComputerCase();
 };
 
 #endif // COMPUTERCASE_HPP
